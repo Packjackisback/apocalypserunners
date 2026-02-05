b07bcead94a0fcfd32afe2ba37c1690c7c5a1afa
packjack <83289186+Packjackisback@users.noreply.github.com>
2026-02-05 13:42:19 -0600

    Somewhat fixing tile rules

--- Assets/Scripts/RoadGenerator.cs
+++ Assets/Scripts/RoadGenerator.cs
@@ -1,23 +1,12 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class RoadGenerator : MonoBehaviour
 {
-    public Tilemap groundTilemap;
     public Tilemap roadTilemap;
 
     [Header("Tile Assets")]
-    public TileBase centerTile;
-    public TileBase manholeTile;
-    public TileBase edgeTop;
-    public TileBase edgeBottom;
-    public TileBase edgeLeft;
-    public TileBase edgeRight;
-    public TileBase cornerTopLeft;
-    public TileBase cornerTopRight;
-    public TileBase cornerBottomLeft;
-    public TileBase cornerBottomRight;
+    public RuleTile roadRuleTile;
 
     [Header("Settings")]
     public int roadWidth = 8;
@@ -35,12 +24,10 @@ public class RoadGenerator : MonoBehaviour
         ChunkRoads myRoads = GetRoadConfig(chunkCoord);
 
         int mid = chunkSize / 2;
-        int hw = roadWidth / 2; // Half-width
+        int hw = roadWidth / 2;
 
         if (myRoads.north || myRoads.south || myRoads.east || myRoads.west)
-        {
             DrawRoadSegment(mid - hw, mid - hw, roadWidth, roadWidth);
-        }
 
         if (myRoads.north) DrawRoadSegment(mid - hw, mid, roadWidth, chunkSize - mid);
         if (myRoads.south) DrawRoadSegment(mid - hw, 0, roadWidth, mid);
@@ -54,30 +41,7 @@ public class RoadGenerator : MonoBehaviour
         {
             for (int y = startY; y < startY + height; y++)
             {
-                bool isLeft = (x == startX);
-                bool isRight = (x == startX + width - 1);
-                bool isBottom = (y == startY);
-                bool isTop = (y == startY + height - 1);
-
-                TileBase selectedTile = centerTile;
-
-
-
-                if (isLeft && isTop) selectedTile = cornerTopLeft;
-                else if (isRight && isTop) selectedTile = cornerTopRight;
-                else if (isLeft && isBottom) selectedTile = cornerBottomLeft;
-                else if (isRight && isBottom) selectedTile = cornerBottomRight;
-                else if (isTop) selectedTile = edgeTop;
-                else if (isBottom) selectedTile = edgeBottom;
-                else if (isLeft) selectedTile = edgeLeft;
-                else if (isRight) selectedTile = edgeRight;
-                else if (GetRNG().Next(0, 100) < holeChance) ;
-
-                Vector3Int pos = new Vector3Int(x, y, 0);
-                if (!roadTilemap.HasTile(pos) || selectedTile != centerTile)
-                {
-                    roadTilemap.SetTile(pos, selectedTile);
-                }
+                roadTilemap.SetTile(new Vector3Int(x, y, 0), roadRuleTile);
             }
         }
     }
@@ -93,18 +57,10 @@ public class RoadGenerator : MonoBehaviour
         };
     }
 
-    private System.Random GetRNG(Vector2Int chunkCoord, Vector2Int direction)
-    {
-        int seed = (chunkCoord.x * 73856093) ^ (chunkCoord.y * 19349663) ^ (direction.x * 83492791) ^ (direction.y * 33492857);
-        return new System.Random(seed);
-    }
-
     private bool GetEdgeHasRoad(Vector2Int chunkCoord, Vector2Int direction)
     {
         int seed = (chunkCoord.x * 73856093) ^ (chunkCoord.y * 19349663) ^ (direction.x * 83492791) ^ (direction.y * 33492857);
         System.Random edgeRng = new System.Random(seed);
         return edgeRng.Next(0, 100) < roadChance;
     }
-
-    public static void ClearCache() { }
 }
